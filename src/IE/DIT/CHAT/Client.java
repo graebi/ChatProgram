0e196488a307d83012cc19ae5861112c30272885
graebi <graebi@web.de>
2016-02-17 22:37:30 +0000

    Server and Client pogrammed - works fine

--- src/IE/DIT/CHAT/Client.java
+++ src/IE/DIT/CHAT/Client.java
@@ -6,8 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,38 +15,37 @@ import java.util.logging.Logger;
  *
  * @author Thorsten
  */
-public class Server {
-    public static void main(String[] args){
+public class Client {
+    
+    public static void main(String[] args) {
         
-        try { 
-            //Creating socket
-            ServerSocket server = new ServerSocket(5555);
-            System.out.println("Chat server started!!");
-            
-            //Connection which is comming from client for server
-            Socket client = server.accept();
+        try {
+            Socket client = new Socket("localhost",5555);
+            System.out.println("Client started!!");
             
             //Data stream to client from server
             OutputStream out = client.getOutputStream();
             PrintWriter writer = new PrintWriter(out);
             
-            //Data stream from client for server
+            //Data stream from client to server
             InputStream in = client.getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             
+            writer.write("Hi server \n");//put "\n" in for reader.readLine to regnise line end
+            writer.flush();
+                        
             String s = null;
             //Read in until end of line
             while ((s = reader.readLine())!=null){
-                writer.write(s +"\n");//put "\n" in for reader.readLine to regnise line end
-                writer.flush();
-                System.out.println("Received from Client: " + s);
+                System.out.println("Received from Server: " + s);
             }//end while loop
             
-            writer.close();
-            reader.close();
             
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+             
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     
